6550e22fcc75100b5bd732ff64fcc433c1bfb329
Xica Felix <xicasfelix@gmail.com>
2021-11-23 13:39:52 -0500

    commit tree 3927e2c24127c877d81873d8f2a0cc71ae8ebcd7

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1= 2;
+let num2 = 31;
+function multiplyI(a,b){
+    return (a* b);
+}
+let multiply = multiplyI(num1, num2);
+
+let random = Math.ceil(Math.random());
+
+let num3 = 23;
+let num4=6;
+let mod = Math.round(num3/num4);
+
+let set= (10,4, 16, 3, 20);
+let max= Math.max(set);
